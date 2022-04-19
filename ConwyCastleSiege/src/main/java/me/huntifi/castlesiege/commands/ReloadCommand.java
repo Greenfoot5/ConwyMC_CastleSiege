b91bdde8661e8cbf1df51b64a84fee7ba392e529
Greenfoot5 <greenfoot89@gmail.com>
2022-04-19 17:46:12 +0100

    Commented the data_types and ReloadCommand

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/ReloadCommand.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/ReloadCommand.java
@@ -1,19 +1,21 @@
 package me.huntifi.castlesiege.commands;
 
 import me.huntifi.castlesiege.Main;
-import me.huntifi.castlesiege.maps.MapController;
-import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.event.Listener;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Adds the option to reload the plugin (does not work properly)
+ */
 public class ReloadCommand implements CommandExecutor {
 
-
+    /**
+     * Invokes the reload command in Main
+     */
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
         Main.instance.reload();
         return true;
     }
--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/data_types/Frame.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/data_types/Frame.java
@@ -5,12 +5,17 @@ import org.bukkit.util.Vector;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Represents a single frame of animation
+ */
 public class Frame {
     public List<Vector> primary_blocks;
     public List<Vector> secondary_blocks;
     public List<Vector> air;
 
+    /**
+     * Creates an empty frame
+     */
     public Frame() {
         primary_blocks = new ArrayList<>();
         secondary_blocks = new ArrayList<>();
