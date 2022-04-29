c4fe7813eef8edd246d490b85a9e04ccb911c0bf
kasteelvuur <kasteelvuur@gmail.com>
2022-04-29 15:29:45 +0200

    Add console private message and reply

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/chat/PrivateMessage.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/chat/PrivateMessage.java
@@ -1,9 +1,7 @@
 package me.huntifi.castlesiege.commands.chat;
 
 import java.util.HashMap;
-import java.util.Map.Entry;
 import java.util.Objects;
-import java.util.UUID;
 
 import me.huntifi.castlesiege.maps.MapController;
 import org.bukkit.Bukkit;
@@ -12,7 +10,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -20,71 +17,84 @@ import org.jetbrains.annotations.NotNull;
  */
 public class PrivateMessage implements CommandExecutor {
 
-	private static final HashMap<UUID, UUID> lastSender = new HashMap<>();
+	private static final HashMap<CommandSender, CommandSender> lastSender = new HashMap<>();
 
 	/**
 	 * Send the provided arguments as a private message to the first argument (player)
-	 * @param sender Source of the command
+	 * @param s Source of the command
 	 * @param cmd Command which was executed
 	 * @param label Alias of the command which was used
 	 * @param args Passed command arguments
 	 * @return false if too few arguments are provided, true otherwise
 	 */
 	@Override
-	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+	public boolean onCommand(@NotNull CommandSender s, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
 		// Command needs a player and a message
 		if (args.length < 2) {
 			return false;
 		}
 
-		// Separate sender, target, and message
-		Player p = (Player) sender;
-		Player t = Bukkit.getPlayer(args[0]);
+		// Separate recipient, and message
+		CommandSender r = args[0].equalsIgnoreCase("console") ?
+				Bukkit.getServer().getConsoleSender() : Bukkit.getPlayer(args[0]);
 		String m = String.join(" ", args).split(" ", 2)[1];
 
 		// Make sure that a correct recipient is supplied
 		// Cannot send message to yourself
-		if (t == null) {
-			p.sendMessage(ChatColor.DARK_RED + "Could not find player: " + ChatColor.RED + args[0]);
+		if (r == null) {
+			s.sendMessage(ChatColor.DARK_RED + "Could not find player: " + ChatColor.RED + args[0]);
 			return true;
-		} else if (Objects.equals(p, t)) {
-			p.sendMessage(ChatColor.RED + "You are not a clown. You are the entire circus.");
+		} else if (Objects.equals(s, r)) {
+			s.sendMessage(ChatColor.RED + "You are not a clown. You are the entire circus.");
 			return true;
 		}
 
 		// Send the message
-		sendMessage(p, t, m);
+		sendMessage(s, r, m);
 
 		return true;
 	}
 
 	/**
 	 * Privately send a message to a player
-	 * @param p The sender of the message
-	 * @param t The receiver of the message
+	 * @param s The sender of the message
+	 * @param r The receiver of the message
 	 * @param m The message that is sent
 	 */
-	public void sendMessage(Player p, Player t, String m) {
+	public void sendMessage(CommandSender s, CommandSender r, String m) {
 		// Get team colors
-		Enum<ChatColor> pColor = MapController.getCurrentMap().getTeam(p.getUniqueId()).primaryChatColor;
-		Enum<ChatColor> tColor = MapController.getCurrentMap().getTeam(t.getUniqueId()).primaryChatColor;
+		Enum<ChatColor> sColor = getTeamColor(s);
+		Enum<ChatColor> rColor = getTeamColor(r);
 
 		// Send messages
-		p.sendMessage(String.format("%sTo %s%s%s: %s%s",
-				ChatColor.GOLD, tColor, t.getName(), ChatColor.GOLD, ChatColor.GRAY, m));
-		t.sendMessage(String.format("%sFrom %s%s%s: %s%s",
-				ChatColor.GOLD, pColor, p.getName(), ChatColor.GOLD, ChatColor.GRAY, m));
+		s.sendMessage(String.format("%sTo %s%s%s: %s%s",
+				ChatColor.GOLD, rColor, r.getName(), ChatColor.GOLD, ChatColor.GRAY, m));
+		r.sendMessage(String.format("%sFrom %s%s%s: %s%s",
+				ChatColor.GOLD, sColor, s.getName(), ChatColor.GOLD, ChatColor.GRAY, m));
 
 		// Set last message sender
-		lastSender.put(t.getUniqueId(), p.getUniqueId());
+		lastSender.put(r, s);
 	}
 
 	/**
-	 * Get the unique id of the last player to send a message to the recipient
-	 * @param uuid The unique id of the recipient
-	 * @return The unique id of the last sender, null if no previous message received
+	 * Get the team color of the command sender
+	 * @param s The command sender to get the team color for
+	 * @return The team color of a player or white
 	 */
-	public UUID getLastSender(UUID uuid) {
-		return lastSender.get(uuid);
+	private Enum<ChatColor> getTeamColor(CommandSender s) {
+		if (s instanceof Player) {
+			Player p = (Player) s;
+			return MapController.getCurrentMap().getTeam(p.getUniqueId()).primaryChatColor;
+		}
+		return ChatColor.WHITE;
+	}
+
+	/**
+	 * Get the last sender of a message to the recipient
+	 * @param s The recipient
+	 * @return The last sender, null if no previous message received
+	 */
+	public CommandSender getLastSender(CommandSender s) {
+		return lastSender.get(s);
 	}
 }
--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/chat/ReplyMessage.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/chat/ReplyMessage.java
@@ -1,15 +1,12 @@
 
 package me.huntifi.castlesiege.commands.chat;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.UUID;
 
 /**
  * Replies to the last message sender
@@ -18,38 +15,33 @@ public class ReplyMessage implements CommandExecutor {
 
 	/**
 	 * Send the provided arguments as a private message to last player to have messaged the command sender
-	 * @param sender Source of the command
+	 * @param s Source of the command
 	 * @param cmd Command which was executed
 	 * @param label Alias of the command which was used
 	 * @param args Passed command arguments
 	 * @return false if no message is provided, true otherwise
 	 */
 	@Override
-	public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+	public boolean onCommand(@NotNull CommandSender s, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
 		// Command needs a message
 		if (args.length == 0) {
 			return false;
 		}
 
-		// Get the sender and receiver
+		// Get the recipient and check if they can be reached
 		PrivateMessage pm = new PrivateMessage();
-		Player p = (Player) sender;
-		UUID uuid = pm.getLastSender(p.getUniqueId());
-		if (uuid == null) {
-			p.sendMessage(ChatColor.DARK_RED + "Nobody has messaged you!");
+		CommandSender r = pm.getLastSender(s);
+		if (r == null) {
+			s.sendMessage(ChatColor.DARK_RED + "Nobody has messaged you!");
 			return true;
-		}
-
-		// Convert receiver to a player
-		Player t = Bukkit.getPlayer(uuid);
-		if (t == null) {
-			p.sendMessage(ChatColor.DARK_RED + "This player is no longer online!");
+		} else if (r instanceof Player && !((Player) r).isOnline()) {
+			s.sendMessage(ChatColor.DARK_RED + "This player is no longer online!");
 			return true;
 		}
 
 		// Send the message
 		String m = String.join(" ", args);
-		pm.sendMessage(p, t, m);
+		pm.sendMessage(s, r, m);
 		return true;
 	}
 }
