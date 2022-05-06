c6b0d4116085f83455816a625a294a1f32dc7368
kasteelvuur <kasteelvuur@gmail.com>
2022-05-06 09:59:30 +0200

    Actually fixed hunger now

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/security/Hunger.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/security/Hunger.java
@@ -11,12 +11,12 @@ public class Hunger implements Runnable {
 	@Override
 	public void run() {
 		for (Player p : Bukkit.getOnlinePlayers()) {
-			String kit = Kit.equippedKits.get(p.getUniqueId()).name;
+			Kit kit = Kit.equippedKits.get(p.getUniqueId());
 			if (kit == null) {
 				continue;
 			}
 
-			if (Objects.equals(kit, "Halberdier")) {
+			if (Objects.equals(kit.name, "Halberdier")) {
 				p.setFoodLevel(4);
 			} else {
 				p.setFoodLevel(p.getFoodLevel() + 1);
