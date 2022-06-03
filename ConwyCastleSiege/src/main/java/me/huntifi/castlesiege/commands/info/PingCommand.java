67033710ae2318c654b0926236da049fb7a9a8ed
Greenfoot5 <greenfoot89@gmail.com>
2022-06-03 18:19:33 +0100

    Fixed Ping imports and Viking damage buffed

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/info/PingCommand.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/info/PingCommand.java
@@ -1,5 +1,7 @@
 package me.huntifi.castlesiege.commands.info;
 
+import me.huntifi.castlesiege.events.chat.Messenger;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/kits/kits/Viking.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/kits/kits/Viking.java
@@ -34,13 +34,13 @@ public class Viking extends Kit implements Listener {
 
         // Weapon
         es.hotbar[0] = ItemCreator.weapon(new ItemStack(Material.IRON_AXE),
-                ChatColor.GREEN + "Giant Battle Axe", null, null, 16);
+                ChatColor.GREEN + "Giant Battle Axe", null, null, 18);
         // Voted Weapon
         es.votedWeapon = new Tuple<>(
                 ItemCreator.weapon(new ItemStack(Material.IRON_AXE),
                         ChatColor.GREEN + "Giant Battle Axe",
                         Collections.singletonList(ChatColor.AQUA + "- voted: +2 damage"),
-                        Collections.singletonList(new Tuple<>(Enchantment.LOOT_BONUS_MOBS, 0)), 18),
+                        Collections.singletonList(new Tuple<>(Enchantment.LOOT_BONUS_MOBS, 0)), 20),
                 0);
 
         // Chestplate
