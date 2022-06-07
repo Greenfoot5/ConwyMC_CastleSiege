11a420a67229b47ef0e84734011f552f0e7a4161
Tasio <tasionogues12345@gmail.com>
2022-06-07 03:53:17 +0100

    changed increment from i += 1 to i++ in /rules

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/info/RulesCommand.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/info/RulesCommand.java
@@ -25,7 +25,7 @@ public class RulesCommand implements CommandExecutor {
 			"No spamming. Do not say the same thing more than once or twice. Do not spam chat with arguments.",
 			"No trolling. We have a zero-tolerance policy for trolling. We know what trolling is and it will not be tolerated.",
 			"Use English in the server chat.",
-			"Do not advertise other servers, unless they are a sub-community of Thedarkage.",
+			"Do not advertise other servers, unless they are a sub-community of TheDarkAge/Conwy.",
 			"Do not use hacked clients. Zero-tolerance policy. You will be banned without warning!",
 			"Do not insult other players and don't be a racist, no hate-speech or bullying.",
 			"Staff have a final say. Do not attempt to argue against punishment decisions once a final decision is given.",
@@ -47,7 +47,7 @@ public class RulesCommand implements CommandExecutor {
 				col = ChatColor.WHITE;
 			} else { col = ChatColor.GRAY; }
 			sb.append(ChatColor.YELLOW + "" + (i+1) + col + ") " + s + "\n");
-			i += 1;
+			i++;
 		}
 		sb.append(ChatColor.WHITE + border);
 		p.sendMessage(sb.toString());
