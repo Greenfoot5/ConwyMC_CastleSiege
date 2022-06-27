7331a24bf49870eb58921059fc71d543ac28b3ec
ConwyMC-admin Huntifi <100959652+AdminHuntifi@users.noreply.github.com>
2022-06-27 14:47:31 +0200

    Spearman's amount of spears and spear throw damage nerfed

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/kits/kits/free_kits/Spearman.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/kits/kits/free_kits/Spearman.java
@@ -49,12 +49,12 @@ public class Spearman extends FreeKit implements Listener {
 		super.heldItemSlot = 0;
 
 		// Weapon
-		es.hotbar[0] = ItemCreator.weapon(new ItemStack(Material.STICK, 5),
+		es.hotbar[0] = ItemCreator.weapon(new ItemStack(Material.STICK, 3),
 				ChatColor.GREEN + "Spear",
 				Collections.singletonList(ChatColor.AQUA + "Right-click to throw a spear."), null, 40);
 		// Voted Weapon
 		es.votedWeapon = new Tuple<>(
-				ItemCreator.weapon(new ItemStack(Material.STICK, 5),
+				ItemCreator.weapon(new ItemStack(Material.STICK, 3),
 						ChatColor.GREEN + "Spear",
 						Arrays.asList(ChatColor.AQUA + "Right-click to throw a spear.",
 								ChatColor.AQUA + "- voted: +2 damage"),
@@ -138,7 +138,7 @@ public class Spearman extends FreeKit implements Listener {
 				Player damages = (Player) arrow.getShooter();
 
 				if (Objects.equals(Kit.equippedKits.get(damages.getUniqueId()).name, name)) {
-					arrow.setDamage(36);
+					arrow.setDamage(33);
 				}
 			}
 		}
