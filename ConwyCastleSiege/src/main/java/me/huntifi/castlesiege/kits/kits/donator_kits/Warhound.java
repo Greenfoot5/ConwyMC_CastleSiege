4054df8b408c717086984c8fe64e90736651c2a4
ConwyMC-admin Huntifi <100959652+AdminHuntifi@users.noreply.github.com>
2022-10-25 18:26:43 +0200

    Berserker and Warhound (BP buffs)

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/kits/kits/donator_kits/Warhound.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/kits/kits/donator_kits/Warhound.java
@@ -41,7 +41,7 @@ public class Warhound extends DonatorKit implements Listener {
      * Set the equipment and attributes of this kit
      */
     public Warhound() {
-        super("Warhound", 155, 10, 10000, 8);
+        super("Warhound", 200, 20, 10000, 10);
         super.canCap = false;
         super.canClimb = false;
         super.canSeeHealth = true;
