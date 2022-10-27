53df63177d03a4aa67adeb3f75bdedd1a1a05019
ConwyMC-admin Huntifi <100959652+AdminHuntifi@users.noreply.github.com>
2022-10-27 13:51:40 +0200

    Crossbow rework

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/kits/kits/donator_kits/Crossbowman.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/kits/kits/donator_kits/Crossbowman.java
@@ -1,6 +1,8 @@
 package me.huntifi.castlesiege.kits.kits.donator_kits;
 
 import me.huntifi.castlesiege.data_types.Tuple;
+import me.huntifi.castlesiege.events.EnderchestEvent;
+import me.huntifi.castlesiege.events.combat.InCombat;
 import me.huntifi.castlesiege.kits.items.EquipmentSet;
 import me.huntifi.castlesiege.kits.items.ItemCreator;
 import me.huntifi.castlesiege.kits.kits.DonatorKit;
@@ -8,18 +10,22 @@ import me.huntifi.castlesiege.kits.kits.Kit;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.ProjectileHitEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import java.util.Collections;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * The crossbowman kit
@@ -29,9 +35,22 @@ public class Crossbowman extends DonatorKit implements Listener {
     /**
      * Set the equipment and attributes of this kit
      */
+
+    public static HashMap<UUID, Boolean> snipers = new HashMap<>();
+
+    //Mobility Mode
+    private ItemStack modeSwitch1;
+    //Sniper Mode
+    private ItemStack modeSwitch2;
+
+    //Mobility Mode has been picked
+    private ItemStack modeSwitchChosen1;
+    //Sniper Mode has been picked
+    private ItemStack modeSwitchChosen2;
+
     public Crossbowman() {
         super("Crossbowman", 250, 8.5, 10000, 10);
-
+        super.kbResistance = 1;
         // Equipment Stuff
         EquipmentSet es = new EquipmentSet();
         super.heldItemSlot = 0;
@@ -68,13 +87,54 @@ public class Crossbowman extends DonatorKit implements Listener {
         es.hotbar[1] = new ItemStack(Material.LADDER, 4);
         es.votedLadders = new Tuple<>(new ItemStack(Material.LADDER, 6), 1);
 
+        // mode swich button
+        modeSwitchChosen1 = ItemCreator.weapon(new ItemStack(Material.LIME_DYE),
+                ChatColor.GREEN + "Mobility Mode", Arrays.asList("",
+                        ChatColor.AQUA + "Mobility mode:",
+                        ChatColor.AQUA + "In this mode crossbowman is faster, ",
+                        ChatColor.AQUA + "has no cooldown and shoots like a normal",
+                        ChatColor.AQUA + "crossbow.", "",
+                        ChatColor.YELLOW + "" + ChatColor.BOLD + "Right click to enable Mobility Mode."),
+                Collections.singletonList(new Tuple<>(Enchantment.LOOT_BONUS_MOBS, 0)), 0);
+
+        modeSwitch2 = ItemCreator.weapon(new ItemStack(Material.YELLOW_DYE),
+                ChatColor.YELLOW + "Sniper Mode", Arrays.asList("",
+                        ChatColor.AQUA + "Sniper mode:",
+                        ChatColor.AQUA + "In this mode crossbowman is slower, ",
+                        ChatColor.AQUA + "has a cooldown and shoots like a sniper",
+                        ChatColor.AQUA + "",
+                        ChatColor.YELLOW + "" + ChatColor.BOLD + "Right click to enable Sniper Mode."),
+                null, 0);
+
+        // mode swich button
+        modeSwitch1 = ItemCreator.weapon(new ItemStack(Material.LIME_DYE),
+                ChatColor.GREEN + "Mobility Mode", Arrays.asList("",
+                        ChatColor.AQUA + "Mobility mode:",
+                        ChatColor.AQUA + "In this mode crossbowman is faster, ",
+                        ChatColor.AQUA + "has no cooldown and shoots like a normal",
+                        ChatColor.AQUA + "crossbow.", "",
+                        ChatColor.YELLOW + "" + ChatColor.BOLD + "Right click to enable Mobility Mode."),
+                null, 0);
+
+        modeSwitchChosen2 = ItemCreator.weapon(new ItemStack(Material.YELLOW_DYE),
+                ChatColor.YELLOW + "Sniper Mode", Arrays.asList("",
+                        ChatColor.AQUA + "Sniper mode:",
+                        ChatColor.AQUA + "In this mode crossbowman is slower, ",
+                        ChatColor.AQUA + "has a cooldown and shoots like a sniper",
+                        ChatColor.AQUA + "",
+                        ChatColor.YELLOW + "" + ChatColor.BOLD + "Right click to enable Sniper Mode."),
+                Collections.singletonList(new Tuple<>(Enchantment.LOOT_BONUS_MOBS, 0)), 0);
+
+        es.hotbar[6] = modeSwitchChosen1;
+        es.hotbar[7] = modeSwitch2;
+
         // Arrows
-        es.hotbar[7] = new ItemStack(Material.ARROW, 48);
+        es.hotbar[5] = new ItemStack(Material.ARROW, 48);
 
         super.equipment = es;
 
-        // Perm Potion Effect
-        super.potionEffects.add(new PotionEffect(PotionEffectType.FAST_DIGGING, 999999, 1));
+        // Potion effects when Mobility Mode
+        super.potionEffects.add(new PotionEffect(PotionEffectType.SPEED, 999999, 0));
 
         // Death Messages
         super.projectileDeathMessage[0] = "Your skull was pierced by ";
@@ -89,7 +149,8 @@ public class Crossbowman extends DonatorKit implements Listener {
      */
     @EventHandler
     public void shootCrossbow(EntityShootBowEvent e) {
-        if (e.isCancelled() || !(e.getEntity() instanceof Player)) {
+        //Multiple checks including if the crossbow is in sniper mode.
+        if (e.isCancelled() || !(e.getEntity() instanceof Player) || !isInSnipingMode(e.getEntity().getUniqueId())) {
             return;
         }
 
@@ -100,11 +161,115 @@ public class Crossbowman extends DonatorKit implements Listener {
                 return;
             }
 
-            p.setCooldown(Material.CROSSBOW, 55);
+            p.setCooldown(Material.CROSSBOW, 70);
             Arrow a = (Arrow) e.getProjectile();
             ((Arrow) e.getProjectile()).setPierceLevel(1);
             a.setKnockbackStrength(1);
             a.setVelocity(p.getLocation().getDirection().normalize().multiply(40));
+        }
+    }
+
+    /**
+     * Destroy a player's brewing stand when they click an enderchest.
+     * @param event The event called when an off-cooldown player interacts with an enderchest
+     */
+    @EventHandler
+    public void onClickEnderchest(EnderchestEvent event) {
+        snipers.put(event.getPlayer().getUniqueId(), false);
+    }
+
+    /**
+     *
+     * @param uuid the uuid of the player to check
+     * @return true if the player is in snipe mode, false if they aren't.
+     */
+    public boolean isInSnipingMode(UUID uuid) {
+        if (snipers.get(uuid) == null) {
+            return false;
+        } else if (snipers.get(uuid) == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Set the arrow-damage of a ranger's arrows
+     * @param e The event called when a player is hit by an arrow
+     */
+    @EventHandler (priority = EventPriority.LOW)
+    public void onArrowHit(ProjectileHitEvent e) {
+        if (e.getEntity() instanceof Arrow &&
+                e.getEntity().getShooter() instanceof Player &&
+                Objects.equals(Kit.equippedKits.get(((Player) e.getEntity().getShooter()).getUniqueId()).name, name)) {
+            if (!isInSnipingMode(((Player) e.getEntity().getShooter()).getUniqueId())) {
+                ((Arrow) e.getEntity()).setDamage(18);
+            }
+        }
+    }
+
+
+    public void onClickGreenDye(Player p) {
+        // mode swich button
+        int cooldown = p.getCooldown(Material.GREEN_DYE);
+        if (cooldown == 0 && isInSnipingMode(p.getUniqueId())) {
+            p.setCooldown(Material.LIME_DYE, 200);
+            p.setCooldown(Material.YELLOW_DYE, 200);
+            p.getInventory().setItem(7, modeSwitch2);
+            p.getInventory().setItem(6, modeSwitchChosen1);
+            // Potion effects when Mobility Mode
+            for (PotionEffect effect : p.getActivePotionEffects()) {
+                if ((effect.getType().getName().equals(PotionEffectType.SLOW.getName()) && effect.getAmplifier() == 2)) {
+                    p.removePotionEffect(effect.getType());
+                    this.potionEffects.remove(effect.getType());
+                }
+            }
+            p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 0));
+            snipers.put(p.getUniqueId(), false);
+        }
+    }
+
+    public void onClickYellowDye(Player p) {
+        int cooldown = p.getCooldown(Material.YELLOW_DYE);
+        if (cooldown == 0 && !isInSnipingMode(p.getUniqueId())) {
+            p.setCooldown(Material.LIME_DYE, 200);
+            p.setCooldown(Material.YELLOW_DYE, 200);
+            p.getInventory().setItem(6, modeSwitch1);
+            p.getInventory().setItem(7, modeSwitchChosen2);
+            // Potion effects when Sniper Mode
+            for (PotionEffect effect : p.getActivePotionEffects()) {
+                if ((effect.getType().getName().equals(PotionEffectType.SPEED.getName()) && effect.getAmplifier() == 0)) {
+                    p.removePotionEffect(effect.getType());
+                    this.potionEffects.remove(effect.getType());
+                }
+            }
+            p.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 999999, 2));
+            snipers.put(p.getUniqueId(), true);
+        }
+    }
+
+
+    /**
+     *
+     * @param e event triggered by right clicking mode switch button.
+     */
+    @EventHandler
+    public void onModeSwitch(PlayerInteractEvent e) {
+        Player p = e.getPlayer();
+        UUID uuid = p.getUniqueId();
+        // Prevent using in lobby
+        if (InCombat.isPlayerInLobby(uuid)) {
+            return;
+        }
+        if (Objects.equals(Kit.equippedKits.get(uuid).name, name)) {
+            if (e.getAction() == Action.RIGHT_CLICK_AIR || e.getAction() == Action.RIGHT_CLICK_BLOCK) {
+                if (p.getInventory().getItemInMainHand().getType().equals(Material.LIME_DYE)) {
+                    onClickGreenDye(p);
+                }
+                if (p.getInventory().getItemInMainHand().getType().equals(Material.YELLOW_DYE)) {
+                    onClickYellowDye(p);
+                }
+            }
         }
     }
 }
