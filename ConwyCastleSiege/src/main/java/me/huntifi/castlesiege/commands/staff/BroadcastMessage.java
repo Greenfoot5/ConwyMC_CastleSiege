2021c16b39b61d916153331248c9dc6e4eb86bed
Greenfoot5 <greenfoot89@gmail.com>
2023-02-22 20:33:24 +0000

    Cleaned up command

--- ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/staff/BroadcastMessage.java
+++ ConwyCastleSiege/src/main/java/me/huntifi/castlesiege/commands/staff/BroadcastMessage.java
@@ -1,6 +1,5 @@
 package me.huntifi.castlesiege.commands.staff;
 
-import me.huntifi.castlesiege.events.chat.Messenger;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -16,53 +15,31 @@ public class BroadcastMessage implements CommandExecutor {
      * Sends a message to all players on the server, this can for example be used to let players know someone donated to the server.
      * I also allowed this to recognise colour codes.
      *
-     * @param s Source of the command
+     * @param sender Source of the command
      * @param cmd Command which was executed
      * @param label Alias of the command which was used
      * @param args Passed command arguments
-     * @return
      */
-
     @Override
-    public boolean onCommand(@NotNull CommandSender s, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
 
         String broadcastPrefix = "§2[§4ConwyMC§2] - §a";
 
-        String message = "";
+        StringBuilder message = new StringBuilder();
 
-                // Command needs a message
-                if (args.length >= 0) {
-
-                    for (int i = 0; i < args.length; i++) {
-
-                        message += args[i] + " ";
-
-                    }
-
-                    if (s instanceof Player) {
-
-                        Player p = (Player) s;
-
-                        if (p.hasPermission("castlesiege.moderator")) {
-
-                            Bukkit.broadcastMessage(broadcastPrefix + ChatColor.translateAlternateColorCodes('&', message));
-
-                        }
-
-                    } else {
-
-                        Bukkit.broadcastMessage(broadcastPrefix + ChatColor.translateAlternateColorCodes('&', message));
-
-                    }
-
-                } else {
-
-                    Messenger.sendError("Right what are you doing... ? Can't send an empty broadcast message can you now?", s);
-                    return false;
-                }
-
-        return true;
-
+        // Command needs a message
+        for (String arg : args) {
+            message.append(arg).append(" ");
         }
 
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            if (p.hasPermission("castlesiege.moderator")) {
+                Bukkit.broadcastMessage(broadcastPrefix + ChatColor.translateAlternateColorCodes('&', message.toString()));
+            }
+        } else {
+            Bukkit.broadcastMessage(broadcastPrefix + ChatColor.translateAlternateColorCodes('&', message.toString()));
+        }
+        return true;
+    }
 }
