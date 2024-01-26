8f115d70aa43fa5068051370d2cb5f63bd048a88
Greenfoot5 <greenfoot89@gmail.com>
2024-01-26 18:45:01 +0000

    Oops

--- src/main/java/me/huntifi/castlesiege/commands/staff/maps/SetMapCommand.java
+++ src/main/java/me/huntifi/castlesiege/commands/staff/maps/SetMapCommand.java
@@ -2,7 +2,6 @@ package me.huntifi.castlesiege.commands.staff.maps;
 
 import me.huntifi.castlesiege.events.chat.Messenger;
 import me.huntifi.castlesiege.maps.MapController;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
