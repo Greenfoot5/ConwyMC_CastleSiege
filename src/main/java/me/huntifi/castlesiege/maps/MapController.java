dc227e1af772944e62582d42488671ef51212f95
Greenfoot5 <greenfoot89@gmail.com>
2024-02-10 19:48:37 +0000

    Removed some Battlepoint parts

--- src/main/java/me/huntifi/castlesiege/maps/MapController.java
+++ src/main/java/me/huntifi/castlesiege/maps/MapController.java
@@ -57,18 +57,12 @@ public class MapController {
 	// Boosters - chances
 	private static final double BASE_BOOSTER_CHANCE = 0.15;
 	private static final double COIN_BOOSTER_CHANCE = 0.35;
-//	 private static final double BATTLEPOINT_BOOSTER_CHANCE = 0.3;
-//	 private static final double KIT_BOOSTER_CHANCE = 0.35; // Not actually used, set for reference
+
 	// Boosters - limits/sub-chances
 	private static final int COIN_BOOSTER_MAX_TIME = 9000;
 	private static final int COIN_BOOSTER_MIN_TIME = 1800;
 	private static final double COIN_BOOSTER_GAUSSIAN_DIV = 2.75;
 	private static final double COIN_BOOSTER_GAUSSIAN_ADD = 3;
-//	 private static final int BP_BOOSTER_MAX_TIME = 2700;
-//	 private static final int BP_BOOSTER_MIN_TIME = 300;
-//	 private static final double BP_BOOSTER_MULT_CHANCE = 0.5;
-//	 private static final double BP_BOOSTER_MAX_MULT = 2.5;
-//	private static final double BP_BOOSTER_MIN_MULT = -1;
 	private static final int KIT_BOOSTER_MAX_TIME = 9000;
 	private static final int KIT_BOOSTER_MIN_TIME = 1800;
 	private static final double KIT_BOOSTER_RANDOM_CHANCE = 0.35;
@@ -228,16 +222,6 @@ public class MapController {
 					Team team = TeamController.getTeam(player.getUniqueId());
 					if (team != null) {
 						player.teleport(team.lobby.spawnPoint);
-					}
-
-					// Refund the player's bp if they didn't die
-					UUID uuid = player.getUniqueId();
-					if (!InCombat.isPlayerInLobby(uuid))
-					{
-						Kit kit = Kit.equippedKits.get(uuid);
-						if (kit instanceof CoinKit) {
-							CoinKit dKit = (CoinKit) kit;
-						}
 					}
 				}
 				InCombat.clearCombat();
@@ -721,6 +705,7 @@ public class MapController {
 	/**
 	 * Checks if a player is a spectator
 	 * @param uuid The uuid of the player to check
+     * @return If the player is a spectator
 	 */
 	public static boolean isSpectator(UUID uuid) {
 		return SpectateCommand.spectators.contains(uuid);
