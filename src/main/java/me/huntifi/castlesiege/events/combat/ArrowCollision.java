4a24e7cc9a5f726947e95a662d761a403cece1f7
Greenfoot5 <greenfoot89@gmail.com>
2024-02-16 14:43:06 +0000

    Fix Arrow Collision Issues

--- src/main/java/me/huntifi/castlesiege/events/combat/ArrowCollision.java
+++ src/main/java/me/huntifi/castlesiege/events/combat/ArrowCollision.java
@@ -33,9 +33,8 @@ public class ArrowCollision implements Listener {
                 Arrow a = defender.launchProjectile(Arrow.class);
                 a.setShooter(attacker);
                 a.setVelocity(thor);
+                e.setCancelled(true);
             }
-
         }
-
     }
 }
