03fad36f6f289e3d3f3553b4b8212d63dbc589d9
Greenfoot5 <greenfoot89@gmail.com>
2024-05-19 16:04:09 +0100

    Cleaned up and improved cannon

--- src/main/java/me/huntifi/castlesiege/maps/objects/Cannon.java
+++ src/main/java/me/huntifi/castlesiege/maps/objects/Cannon.java
@@ -1,15 +1,15 @@
 package me.huntifi.castlesiege.maps.objects;
 
 import me.huntifi.castlesiege.Main;
-import me.huntifi.castlesiege.events.chat.Messenger;
 import me.huntifi.castlesiege.events.combat.InCombat;
 import me.huntifi.castlesiege.kits.kits.Kit;
+import me.huntifi.castlesiege.kits.kits.map_kits.Artillerist;
 import me.huntifi.castlesiege.structures.SchematicSpawner;
+import me.huntifi.conwymc.util.Messenger;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
@@ -18,114 +18,73 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.util.Vector;
 
 import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Objects;
 import java.util.UUID;
 
+/**
+ * Represents a cannon
+ */
 public class Cannon implements Listener {
 
-    // General variables used by the catapult
+    // General variables used by the cannon
     private final String direction;
-    private final World world;
-
-    // Location and names of the schematics used for the different cannon states
     private final Location schematicLocation;
-    // Location where the recoil happens
     private final Location recoil;
-    private final String schematicReady;
-    private final String schematicReloading;
-    private final String schematicShot;
 
     // Booleans used to represent the catapult state
-    private boolean canRefill = false;
-    private boolean canShoot = true;
+    private STATE state = STATE.Ready;
 
-    // Variables used for operating the catapult
+    // Variables used for operating the cannon
     private double vertical_multiplier = 1.0;
-    private final double aimHorizontal;
-    private final double aimVertical;
-    private final Location button;
+    private final double yaw;
+    private final double pitch;
 
     // Variables used for shooting the cannon
-    private final Location projectile;
-    private final Location sound;
+    private final Location barrel;
     private Player shooter;
-    private Snowball snowball;
+    private Snowball projectile;
 
     /**
      * Create a new catapult
-     * @param world The name of the world in which the catapult is location
      * @param direction The direction in which the catapult is facing
      * @param location The base location for the catapult
+     * @param yaw The yaw (left/right) to fire the cannonball from.
+     * @param pitch # The pitch (vertical) to fire the cannonball
      */
-    public Cannon(String world, String direction, Vector location, double horizontal, double power) {
-        // Set direction independent variables
-        this.world = Bukkit.getWorld(world);
-        if (this.world == null)
-            throw new IllegalArgumentException("The world " + world + " does not exist!");
+    public Cannon(String direction, Location location, double yaw, double pitch) {
         this.direction = direction.toLowerCase();
-        this.schematicLocation = location.toLocation(this.world);
-        this.aimHorizontal = horizontal;
-        this.aimVertical = power;
+        this.schematicLocation = location;
+        this.yaw = yaw;
+        this.pitch = pitch;
 
         // Set direction dependent variables
         switch (this.direction) {
             case "north":
                 // Locations
-                this.button = new Vector(0, 0, 0).add(location).toLocation(this.world);
-                this.sound = new Vector(0, 1, -4).add(location).toLocation(this.world);
-                this.projectile = new Vector(0, 0, -4).add(location).toLocation(this.world);
-                this.recoil = new Vector(0, 0, 1).add(location).toLocation(this.world);
-
-                // Schematics
-                this.schematicReloading = "Cannon_Reloading_North";
-                this.schematicShot = "Cannon_Shot_North";
-                this.schematicReady = "Cannon_Ready_North";
+                this.barrel = location.add(0, 0, -4);
+                this.recoil = location.add(0, 0, 1);
                 break;
 
             case "east":
                 // Locations
-                this.button = new Vector(0, 0, 0).add(location).toLocation(this.world);
-                this.sound = new Vector(4, 1, 0).add(location).toLocation(this.world);
-                this.projectile = new Vector(4, 0, 0).add(location).toLocation(this.world);
-                this.recoil = new Vector(-1, 0, 0).add(location).toLocation(this.world);
-
-                // Schematics
-                this.schematicReloading = "Cannon_Reloading_East";
-                this.schematicShot = "Cannon_Shot_East";
-                this.schematicReady = "Cannon_Ready_East";
+                this.barrel = location.add(4, 0, 0);
+                this.recoil = location.add(-1, 0, 0);
                 break;
 
             case "south":
                 // Locations
-                this.button = new Vector(0, 0, 0).add(location).toLocation(this.world);
-                this.sound = new Vector(0, 1, 4).add(location).toLocation(this.world);
-                this.projectile = new Vector(0, 0, 4).add(location).toLocation(this.world);
-                this.recoil = new Vector(0, 0, -1).add(location).toLocation(this.world);
-
-                // Schematics
-                this.schematicReloading = "Cannon_Reloading_South";
-                this.schematicShot = "Cannon_Shot_South";
-                this.schematicReady = "Cannon_Ready_South";
+                this.barrel = location.add(0, 0, 4);
+                this.recoil = location.add(0, 0, -1);
                 break;
 
             case "west":
                 // Locations
-                this.button = new Vector(0, 0, 0).add(location).toLocation(this.world);
-                this.sound = new Vector(-4, 1, 0).add(location).toLocation(this.world);
-                this.projectile = new Vector(-4, 0, 0).add(location).toLocation(this.world);
-                this.recoil = new Vector(1, 0, 0).add(location).toLocation(this.world);
-
-                // Schematics
-                schematicReloading = "Cannon_Reloading_West";
-                schematicShot = "Cannon_Shot_West";
-                this.schematicReady = "Cannon_Ready_West";
+                this.barrel = location.add(-4, 0, 0);
+                this.recoil = location.add(1, 0, 0);
                 break;
 
             default:
@@ -150,18 +109,10 @@ public class Cannon implements Listener {
 
         // Make sure the button belongs to this cannon
         Player player = event.getPlayer();
-        if (Objects.equals(player.getWorld(), world) && clickedBlock.getLocation().distance(button) == 0) {
-
-            // Check if the cannon can be shot
-            if (canShoot) {
-                shooter = player;
-                shoot();
-            } else {
-                Messenger.sendActionError("This cannon is still reloading or has not been reloaded yet.", player);
-                event.setCancelled(true);
-            }
+        if (clickedBlock.getLocation().distance(schematicLocation) == 0) {
+            shooter = player;
+            shoot();
         }
-
     }
 
     /**
@@ -169,20 +120,15 @@ public class Cannon implements Listener {
      */
     private void shoot() {
         // Animate the shot and play the shooting sound
-        SchematicSpawner.spawnSchematic(recoil, schematicShot);
-        world.playSound(sound, Sound.ENTITY_GENERIC_EXPLODE, 5, 1);
+        SchematicSpawner.spawnSchematic(recoil, "Cannon_Shot_" + direction);
+        barrel.getWorld().playSound(barrel, Sound.ENTITY_GENERIC_EXPLODE, 5, 1);
 
         // Perform the shot
         launchProjectile();
-        canShoot = false;
+        state = STATE.Shot;
 
         // Start the 10s timer for the catapult coming back down
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-                reposition();
-            }
-        }.runTaskLater(Main.plugin, 200);
+        Bukkit.getScheduler().runTaskLater(Main.plugin, this::reposition, 200);
     }
 
     /**
@@ -190,20 +136,19 @@ public class Cannon implements Listener {
      */
     private void reposition() {
         // Animate the reposition and play the reposition sound
-        SchematicSpawner.spawnSchematic(schematicLocation, schematicReloading);
-        world.playSound(sound, Sound.BLOCK_DISPENSER_DISPENSE, 5, 1);
-        canRefill = true;
+        SchematicSpawner.spawnSchematic(schematicLocation, "Cannon_Reloading_" + direction);
+        barrel.getWorld().playSound(barrel, Sound.BLOCK_DISPENSER_DISPENSE, 5, 1);
+        state = STATE.Reloading;
     }
 
     /**
      * Refill the cannon supposedly and allow it to be shot again
      */
-    private void refill() {
-        if (canRefill) {
-            SchematicSpawner.spawnSchematic(schematicLocation, schematicReady);
+    private void addBall() {
+        if (state == STATE.Reloading) {
+            SchematicSpawner.spawnSchematic(schematicLocation, "Cannon_Ready_" + direction);
             // Perform the logical changes
-            canShoot = true;
-            canRefill = false;
+            state = STATE.Ready;
         }
     }
 
@@ -211,42 +156,42 @@ public class Cannon implements Listener {
      * Launch the catapult's projectile
      */
     public void launchProjectile() {
-        // Spawn a falling cobblestone block riding a snowball
-        snowball = world.spawn(projectile, Snowball.class);
+        // Spawn a falling cobblestone block riding a projectile
+        projectile = barrel.getWorld().spawn(barrel, Snowball.class);
 
-        // Set the snowball's velocity
+        // Set the projectile's velocity
         Vector vector = new Vector();
         switch (direction) {
             case "north":
-                vector = new Vector(aimHorizontal, aimVertical * vertical_multiplier, -30 - aimVertical * vertical_multiplier);
+                vector = new Vector(yaw, pitch * vertical_multiplier, -30 - pitch * vertical_multiplier);
                 break;
             case "east":
-                vector = new Vector(30 + aimVertical * vertical_multiplier, aimVertical, aimHorizontal);
+                vector = new Vector(30 + pitch * vertical_multiplier, pitch, yaw);
                 break;
             case "south":
-                vector = new Vector(-aimHorizontal, aimVertical * vertical_multiplier, 30 + aimVertical * vertical_multiplier);
+                vector = new Vector(-yaw, pitch * vertical_multiplier, 30 + pitch * vertical_multiplier);
                 break;
             case "west":
-                vector = new Vector(-30 - aimVertical * vertical_multiplier, aimVertical * vertical_multiplier, -aimHorizontal);
+                vector = new Vector(-30 - pitch * vertical_multiplier, pitch * vertical_multiplier, -yaw);
                 break;
         }
-        vertical_multiplier = 1.0;
-        snowball.setVelocity(vector.normalize().multiply(3.5));
+        vertical_multiplier = 0.0;
+        projectile.setVelocity(vector.normalize().multiply(3.5));
     }
 
     /**
      * Delete the cobblestone and create an explosion on impact
-     * @param event The event called when the snowball hits something
+     * @param event The event called when the projectile hits something
      */
     @EventHandler
     public void onImpact(ProjectileHitEvent event) {
-        if (Objects.equals(snowball, event.getEntity())) {
-            world.createExplosion(snowball.getLocation(), 2.5F, false, true, shooter);
+        if (Objects.equals(projectile, event.getEntity())) {
+            schematicLocation.getWorld().createExplosion(projectile.getLocation(), 3F, false, true, shooter);
         }
     }
 
     /**
-     * Allows artillerists to refill cannons
+     * Allows artillerists to addBall cannons
      * @param event The event called when a block is placed
      */
     @EventHandler
@@ -255,41 +200,62 @@ public class Cannon implements Listener {
         UUID uuid = player.getUniqueId();
         Block block = event.getClickedBlock();
         ItemStack item = player.getInventory().getItemInMainHand();
-        int cooldown = player.getCooldown(Material.FIREWORK_STAR);
-        int cooldown2 = player.getCooldown(Material.GUNPOWDER);
-        if (block == null || InCombat.isPlayerInLobby(uuid)) {
+        int ballCooldown = player.getCooldown(Material.PLAYER_HEAD);
+        int powderCooldown = player.getCooldown(Material.GUNPOWDER);
+
+        // If the player isn't clicking on the right thing, we don't care
+        if (block == null || InCombat.isPlayerInLobby(uuid)
+        || block.getLocation().distance(schematicLocation) != 0
+        || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             return;
         }
 
-        // Ensure that an engineer placed a block in the catapult's bucket
-        if (Objects.equals(Kit.equippedKits.get(uuid).name, "Artillerist")
-                && Objects.equals(player.getWorld(), world) && Objects.equals(event.getHand(), EquipmentSlot.HAND)) {
-            if (block.getLocation().distance(button) == 0) {
-                if (player.getInventory().getItemInMainHand().getType() == Material.FIREWORK_STAR) {
-                    if (event.getAction() == Action.RIGHT_CLICK_BLOCK
-                            && cooldown == 0) {
-                        if (!canRefill) {
-                            Messenger.sendActionError("The cannon already has a projectile ready!", player);
-                        } else {
-                            // Perform the refill on the next tick to prevent overwriting the cobblestone slab
-                            refill();
-                            item.setAmount(item.getAmount() - 1);
-                            player.setCooldown(Material.FIREWORK_STAR, 20);
-                        }
-                    }
-                } else if (player.getInventory().getItemInMainHand().getType() == Material.GUNPOWDER) {
-                    if (event.getAction() == Action.RIGHT_CLICK_BLOCK && cooldown2 == 0) {
-                        if (!canShoot && event.getClickedBlock().getType().equals(Material.TRIPWIRE_HOOK)) {
-                            if (vertical_multiplier < 5) {
-                                vertical_multiplier += 0.1;
-                                player.setCooldown(Material.GUNPOWDER, 10);
-                                item.setAmount(item.getAmount() - 1);
-                                Messenger.sendActionInfo("Contains " + new DecimalFormat("0.0").format(vertical_multiplier) + "/Max 5.0 gunpowder", player);
-                            }
-                        }
-                    }
-                }
-            }
+        // Only Arillerist can do this
+        if (!Objects.equals(Kit.equippedKits.get(uuid).name, new Artillerist().name)) {
+            Messenger.sendActionError("Only Arillerists can reload cannons!", player);
         }
+
+        // Player is adding a cannon bal
+        if (player.getInventory().getItemInMainHand().getType() == Material.PLAYER_HEAD
+                && block.getType().equals(Material.TRIPWIRE_HOOK)
+                && ballCooldown == 0) {
+
+            if (state == STATE.Ready) {
+                Messenger.sendActionError("The cannon already has a projectile ready!", player);
+                return;
+            } else if (vertical_multiplier == 0) {
+                Messenger.sendActionError("The cannon need gunpowder first!", player);
+                return;
+            }
+
+            // Perform the addBall on the next tick to prevent overwriting the cobblestone slab
+            addBall();
+            item.setAmount(item.getAmount() - 1);
+            player.setCooldown(Material.FIREWORK_STAR, 20);
+            return;
+        }
+
+        if (player.getInventory().getItemInMainHand().getType() == Material.GUNPOWDER
+                && block.getType().equals(Material.TRIPWIRE_HOOK)
+                && powderCooldown == 0) {
+            if (vertical_multiplier == 0)
+                vertical_multiplier = 1;
+            else if (vertical_multiplier < 3)
+                vertical_multiplier += 0.2;
+            else {
+                Messenger.sendActionError("Cannon is full of gunpowder!", player);
+                return;
+            }
+
+            player.setCooldown(Material.GUNPOWDER, 5);
+            item.setAmount(item.getAmount() - 1);
+            Messenger.sendActionInfo("Contains " + new DecimalFormat("0.0").format(vertical_multiplier) + "/3.0 gunpowder", player);
+        }
+    }
+
+    private enum STATE {
+        Ready,
+        Shot,
+        Reloading
     }
 }
