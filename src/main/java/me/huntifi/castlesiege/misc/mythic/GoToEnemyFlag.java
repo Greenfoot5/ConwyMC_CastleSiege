9604ae64215c65a71c267505ac10e2227ca0b2f5
Greenfoot5 <greenfoot89@gmail.com>
2024-07-30 00:04:10 +0100

    Mobs now teleport every 10s to a flag's spawn point

--- src/main/java/me/huntifi/castlesiege/misc/mythic/GoToEnemyFlag.java
+++ src/main/java/me/huntifi/castlesiege/misc/mythic/GoToEnemyFlag.java
@@ -1,31 +1,37 @@
 package me.huntifi.castlesiege.misc.mythic;
 
-import io.lumine.mythic.api.skills.*;
-import io.lumine.mythic.bukkit.BukkitAPIHelper;
+import io.lumine.mythic.api.skills.IMetaSkill;
+import io.lumine.mythic.api.skills.SkillCaster;
+import io.lumine.mythic.api.skills.SkillMetadata;
+import io.lumine.mythic.api.skills.SkillResult;
+import io.lumine.mythic.api.skills.SkillTrigger;
 import io.lumine.mythic.bukkit.BukkitAdapter;
 import io.lumine.mythic.bukkit.MythicBukkit;
-import io.lumine.mythic.core.mobs.ActiveMob;
+import io.lumine.mythic.core.skills.SkillExecutor;
+import io.lumine.mythic.core.skills.SkillMechanic;
 import io.lumine.mythic.core.skills.SkillMetadataImpl;
 import me.huntifi.castlesiege.maps.MapController;
 import me.huntifi.castlesiege.maps.objects.Flag;
 import org.bukkit.entity.Entity;
 
-import java.util.Collections;
-import java.util.Optional;
 import java.util.Random;
-import java.util.function.Consumer;
 
 public class GoToEnemyFlag implements IMetaSkill {
     @Override
     public SkillResult cast(SkillMetadata skillMetadata) {
+        // Get variables
         SkillCaster caster = skillMetadata.getCaster();
         Entity bukkitEntity = BukkitAdapter.adapt(caster.getEntity());
         Random random = new Random();
         Flag flag = MapController.getCurrentMap().flags[random.nextInt(MapController.getCurrentMap().flags.length)];
         System.out.println("Casing GTEF to " + flag.getName());
-        SkillMetadata meta = new SkillMetadataImpl(SkillTrigger.get("onSpawn"), caster, caster.getEntity());
-        Consumer<SkillMetadata> consumer = metadata -> metadata.setLocationTarget(BukkitAdapter.adapt(flag.getSpawnPoint()));
-        System.out.println((new BukkitAPIHelper()).castSkill(bukkitEntity, "goto", consumer));
+
+        // Setup for casting skill
+        SkillMetadata meta = new SkillMetadataImpl(SkillTrigger.get("onTimer"), caster, caster.getEntity());
+        meta.setLocationTarget(BukkitAdapter.adapt(flag.getSpawnPoint()));
+        //meta.getVariables().put();
+        SkillMechanic example = new SkillExecutor(MythicBukkit.inst()).getMechanic("teleport");
+        System.out.println(example.execute(meta));
         return SkillResult.SUCCESS;
     }
 }
