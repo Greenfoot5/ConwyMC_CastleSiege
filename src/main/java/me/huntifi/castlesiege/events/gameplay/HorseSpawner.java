c7cd7c34fc16b8d2e536aae403d7776276d7a3f6
Greenfoot5 <greenfoot89@gmail.com>
2024-12-30 11:10:13 +0000

    Reduced duplicates in Horse

--- src/main/java/me/huntifi/castlesiege/events/gameplay/HorseSpawner.java
+++ src/main/java/me/huntifi/castlesiege/events/gameplay/HorseSpawner.java
@@ -4,10 +4,12 @@ import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeInstance;
+import org.bukkit.entity.AbstractHorse;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.SkeletonHorse;
+import org.bukkit.entity.ZombieHorse;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.jetbrains.annotations.Nullable;
@@ -60,102 +62,57 @@ class HorseSpawner {
      * @param p The player
      */
     public void spawn(Player p, String type) {
+        AbstractHorse horse;
         if (Objects.equals(type, "horse") || Objects.equals(type, "regular")) {
             // Spawn horse
-            Horse horse = (Horse) p.getWorld().spawnEntity(p.getLocation(), EntityType.HORSE);
-            horse.setTamed(true);
-            horse.setOwner(p);
-            horse.setAdult();
-            horse.addPassenger(p);
-
-            // Set attributes
-            AttributeInstance healthAttribute = horse.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-            AttributeInstance kbAttribute = horse.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE);
-            AttributeInstance speedAttribute = horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
-            AttributeInstance jumpAttribute = horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH);
-
-            assert healthAttribute != null;
-            assert kbAttribute != null;
-            assert speedAttribute != null;
-            assert jumpAttribute != null;
-
-            healthAttribute.setBaseValue(health);
-            horse.setHealth(health);
-            kbAttribute.setBaseValue(knockback);
-            speedAttribute.setBaseValue(speed);
-            jumpAttribute.setBaseValue(jump);
-
-            // Give items
-            horse.getInventory().setSaddle(new ItemStack(Material.SADDLE));
-            horse.getInventory().setArmor(new ItemStack(armor));
-
+            horse = (Horse) p.getWorld().spawnEntity(p.getLocation(), EntityType.HORSE);
+            ((Horse) horse).getInventory().setArmor(new ItemStack(armor));
             // Apply potion effects
             horse.addPotionEffects(effects);
+
 
         } else if (Objects.equals(type, "skeletal") || Objects.equals(type, "skeleton")) {
             // Spawn horse
-            SkeletonHorse horse = (SkeletonHorse) p.getWorld().spawnEntity(p.getLocation(), EntityType.SKELETON_HORSE);
-            horse.setTamed(true);
-            horse.setOwner(p);
-            horse.setAdult();
-            horse.addPassenger(p);
+            horse = (SkeletonHorse) p.getWorld().spawnEntity(p.getLocation(), EntityType.SKELETON_HORSE);
 
-            // Set attributes
-            AttributeInstance healthAttribute = horse.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-            AttributeInstance kbAttribute = horse.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE);
-            AttributeInstance speedAttribute = horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
-            AttributeInstance jumpAttribute = horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH);
-
-            assert healthAttribute != null;
-            assert kbAttribute != null;
-            assert speedAttribute != null;
-            assert jumpAttribute != null;
-
-            healthAttribute.setBaseValue(health);
-            horse.setHealth(health);
-            kbAttribute.setBaseValue(knockback);
-            speedAttribute.setBaseValue(speed);
-            jumpAttribute.setBaseValue(jump);
-
-            // Give items
-            horse.getInventory().setSaddle(new ItemStack(Material.SADDLE));
-
-            // Apply potion effects
-            horse.addPotionEffects(effects);
             p.getWorld().playSound(p.getLocation(), Sound.ENTITY_HORSE_DEATH, 2, 0.25f);
             p.getWorld().playSound(p.getLocation(), Sound.ENTITY_ZOMBIE_INFECT, 2, 0.25f);
             p.getWorld().playSound(p.getLocation(), Sound.ENTITY_PHANTOM_DEATH, 2, 0.25f);
             p.getWorld().playSound(p.getLocation(), Sound.ENTITY_GHAST_DEATH, 2, 0.25f);
+
         } else if (Objects.equals(type, "zombified") || Objects.equals(type, "undead") || Objects.equals(type, "zombie")) {
             // Spawn horse
-            SkeletonHorse horse = (SkeletonHorse) p.getWorld().spawnEntity(p.getLocation(), EntityType.ZOMBIE_HORSE);
-            horse.setTamed(true);
-            horse.setOwner(p);
-            horse.setAdult();
-            horse.addPassenger(p);
-
-            // Set attributes
-            AttributeInstance healthAttribute = horse.getAttribute(Attribute.GENERIC_MAX_HEALTH);
-            AttributeInstance kbAttribute = horse.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE);
-            AttributeInstance speedAttribute = horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
-            AttributeInstance jumpAttribute = horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH);
-
-            assert healthAttribute != null;
-            assert kbAttribute != null;
-            assert speedAttribute != null;
-            assert jumpAttribute != null;
-
-            healthAttribute.setBaseValue(health);
-            horse.setHealth(health);
-            kbAttribute.setBaseValue(knockback);
-            speedAttribute.setBaseValue(speed);
-            jumpAttribute.setBaseValue(jump);
-
-            // Give items
-            horse.getInventory().setSaddle(new ItemStack(Material.SADDLE));
-
-            // Apply potion effects
-            horse.addPotionEffects(effects);
+            horse = (ZombieHorse) p.getWorld().spawnEntity(p.getLocation(), EntityType.ZOMBIE_HORSE);
+        } else {
+            return;
         }
+
+        horse.setTamed(true);
+        horse.setOwner(p);
+        horse.setAdult();
+        horse.addPassenger(p);
+
+        // Set attributes
+        AttributeInstance healthAttribute = horse.getAttribute(Attribute.GENERIC_MAX_HEALTH);
+        AttributeInstance kbAttribute = horse.getAttribute(Attribute.GENERIC_KNOCKBACK_RESISTANCE);
+        AttributeInstance speedAttribute = horse.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED);
+        AttributeInstance jumpAttribute = horse.getAttribute(Attribute.HORSE_JUMP_STRENGTH);
+
+        assert healthAttribute != null;
+        assert kbAttribute != null;
+        assert speedAttribute != null;
+        assert jumpAttribute != null;
+
+        healthAttribute.setBaseValue(health);
+        horse.setHealth(health);
+        kbAttribute.setBaseValue(knockback);
+        speedAttribute.setBaseValue(speed);
+        jumpAttribute.setBaseValue(jump);
+
+        // Give items
+        horse.getInventory().setSaddle(new ItemStack(Material.SADDLE));
+
+        // Apply potion effects
+        horse.addPotionEffects(effects);
     }
 }
