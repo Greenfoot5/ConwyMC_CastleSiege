5683970c2d0c34a70e2796708be3cd135bd48c02
Greenfoot5 <greenfoot89@gmail.com>
2025-01-08 22:24:17 +0000

    MapBorder to record

--- src/main/java/me/greenfoot5/castlesiege/data_types/MapBorder.java
+++ src/main/java/me/greenfoot5/castlesiege/data_types/MapBorder.java
@@ -3,24 +3,16 @@ package me.greenfoot5.castlesiege.data_types;
 /**
  * Represents a map's border
  */
-public class MapBorder {
-
-    public final double north;
-    public final double east;
-    public final double south;
-    public final double west;
+public record MapBorder(double north, double east, double south, double west) {
 
     /**
      * Create a map border with the corresponding coordinates.
+     *
      * @param north The north side of the border
-     * @param east The east side of the border
+     * @param east  The east side of the border
      * @param south The south side of the border
-     * @param west The west side of the border
+     * @param west  The west side of the border
      */
-    public MapBorder(double north, double east, double south, double west) {
-        this.north = north;
-        this.east = east;
-        this.south = south;
-        this.west = west;
+    public MapBorder {
     }
 }
