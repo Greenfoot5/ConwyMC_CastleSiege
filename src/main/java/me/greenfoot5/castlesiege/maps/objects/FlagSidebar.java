45498326a757791b3ddff18bf59d886ecadccd26
Greenfoot5 <greenfoot89@gmail.com>
2025-01-10 14:24:56 +0000

    Added pagination to scoreboard

--- src/main/java/me/greenfoot5/castlesiege/maps/objects/FlagSidebar.java
+++ src/main/java/me/greenfoot5/castlesiege/maps/objects/FlagSidebar.java
@@ -12,10 +12,14 @@ import net.kyori.adventure.text.format.TextDecoration;
 import net.megavex.scoreboardlibrary.api.sidebar.Sidebar;
 import net.megavex.scoreboardlibrary.api.sidebar.component.ComponentSidebarLayout;
 import net.megavex.scoreboardlibrary.api.sidebar.component.SidebarComponent;
+import net.megavex.scoreboardlibrary.api.sidebar.component.animation.CollectionSidebarAnimation;
 import net.megavex.scoreboardlibrary.api.sidebar.component.animation.SidebarAnimation;
 import org.apache.commons.lang3.ArrayUtils;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * The scoreboard displaying all the flags and their capture status
@@ -24,6 +28,8 @@ public class FlagSidebar {
     private final Sidebar sidebar;
     private final ComponentSidebarLayout componentSidebar;
     private final SidebarAnimation<Component> titleAnimation;
+    private final SidebarAnimation<SidebarComponent> pagesAnimation;
+    private int pagesTicks = 0;
 
     /**
      * @param sidebar The sidebar to create the flags scoreboard on
@@ -34,52 +40,122 @@ public class FlagSidebar {
         this.titleAnimation = Scoreboard.createGradientAnimation(Component.text("Mode: ")
                 .append(Component.text(MapController.getCurrentMap().gamemode.toString(),
                         Style.style(TextDecoration.BOLD))));
-
         SidebarComponent title = SidebarComponent.animatedLine(titleAnimation);
 
-        // Map Info
-        SidebarComponent.Builder lines = SidebarComponent.builder()
-                .addDynamicLine(() -> {
-                    Component time = Scoreboard.getTimeText();
-                    return time.color(NamedTextColor.GOLD).decorate(TextDecoration.BOLD);
-                })
-                .addStaticLine(Component.text("Map: ", NamedTextColor.GOLD).decorate(TextDecoration.BOLD)
-                        .append(Component.text(MapController.getCurrentMap().name, NamedTextColor.GREEN)
-                                .decoration(TextDecoration.BOLD, false)))
-                // 30 dashes to define minimum width
-                .addStaticLine(Component.text("━━━━━━━━━━━━━━━━━━━━━━━━━", NamedTextColor.DARK_GRAY).decorate(TextDecoration.STRIKETHROUGH));
+        // Calculate the number of lines needed to display stuff
+        int availableLines = sidebar.maxLines() - 3;
 
+        // Calculate how many lines we need to display cores, lives & flags
+        int requiredLines = getRequiredLines();
 
-        // Core Map
+        // If we need multiple pages we'll need to display the page number
+        final int pages;
+        if (requiredLines > availableLines) {
+            availableLines -= 1;
+            pages = requiredLines / availableLines + (requiredLines % availableLines == 0 ? 0 : 1);
+        } else {
+            pages = 1;
+        }
+
+        int coreIndex = 0;
+        int lifeIndex = 0;
+        int flagIndex = 0;
+
+        List<SidebarComponent> sidebarPages = new ArrayList<>();
+        for (int page = 0; page < pages; page++) {
+            // Map Info
+            SidebarComponent.Builder lines = SidebarComponent.builder()
+                    .addDynamicLine(() -> {
+                        Component time = Scoreboard.getTimeText();
+                        return time.color(NamedTextColor.GOLD).decorate(TextDecoration.BOLD);
+                    })
+                    .addStaticLine(Component.text("Map: ", NamedTextColor.GOLD).decorate(TextDecoration.BOLD)
+                            .append(Component.text(MapController.getCurrentMap().name, NamedTextColor.GREEN)
+                                    .decoration(TextDecoration.BOLD, false)))
+                    // 30 dashes to define minimum width
+                    .addStaticLine(Component.text("━━━━━━━━━━━━━━━━━━━━━━━━━", NamedTextColor.DARK_GRAY).decorate(TextDecoration.STRIKETHROUGH));
+
+            int linesLeft = availableLines;
+
+            // Core Map
+            if (MapController.getCurrentMap() instanceof CoreMap coreMap && coreIndex < coreMap.getCores().length) {
+                lines.addStaticLine(Component.text("Cores", NamedTextColor.WHITE).decorate(TextDecoration.BOLD));
+                linesLeft--;
+
+                // Display the core scoreboard
+                while (coreIndex < coreMap.getCores().length && linesLeft > 0) {
+                    lines.addComponent(coreMap.getCore(coreIndex));
+                    linesLeft--;
+                    coreIndex++;
+                }
+
+                if (MapController.getCurrentMap().flags.length > 0 && linesLeft > 5) {
+                    lines.addBlankLine();
+                    linesLeft--;
+                }
+            } else if (MapController.getCurrentMap().gamemode == Gamemode.Assault && lifeIndex < MapController.getCurrentMap().teams.length) {
+                lines.addStaticLine(Component.text("Lives", NamedTextColor.WHITE).decorate(TextDecoration.BOLD));
+                linesLeft--;
+
+                while (lifeIndex < MapController.getCurrentMap().teams.length && linesLeft > 0) {
+                    if (MapController.getCurrentMap().teams[lifeIndex].getLives() >= 0) {
+                        lines.addComponent(MapController.getCurrentMap().teams[lifeIndex]);
+                        linesLeft--;
+                    }
+                    lifeIndex++;
+                }
+
+                if (MapController.getCurrentMap().flags.length > 0 && linesLeft > 5) {
+                    lines.addBlankLine();
+                    linesLeft--;
+                }
+            }
+
+            // Flip the order of flags, so they're in a more logical order
+            Flag[] reversedFlags = MapController.getCurrentMap().flags.clone();
+            ArrayUtils.reverse(reversedFlags);
+            // Display the flag scoreboard
+            while (flagIndex < reversedFlags.length && linesLeft > 0) {
+                if (reversedFlags[flagIndex].isActive()) {
+                    lines.addComponent(reversedFlags[flagIndex]);
+                    linesLeft--;
+                    flagIndex++;
+                }
+            }
+
+            if (pages > 1) {
+                lines.addStaticLine(Component.text("Page " + (page + 1) + " of " + pages, NamedTextColor.WHITE));
+            }
+
+            sidebarPages.add(lines.build());
+        }
+
+        pagesAnimation = new CollectionSidebarAnimation<>(sidebarPages);
+        SidebarComponent paginatedComponent = SidebarComponent.animatedComponent(pagesAnimation);
+        this.componentSidebar = new ComponentSidebarLayout(title, paginatedComponent);
+    }
+
+    private static int getRequiredLines() {
+        int requiredLines = 0;
         if (MapController.getCurrentMap() instanceof CoreMap coreMap) {
-            lines.addStaticLine(Component.text("Cores", NamedTextColor.WHITE).decorate(TextDecoration.BOLD));
-            // Display the core scoreboard
-            for (Core core : coreMap.getCores()) {
-                lines.addComponent(core);
-            }
-
-            if (MapController.getCurrentMap().flags.length > 0)
-                lines.addBlankLine();
+            requiredLines += coreMap.getCores().length + 1;
+            if (coreMap.flags.length > 0)
+                requiredLines++;
         } else if (MapController.getCurrentMap().gamemode == Gamemode.Assault) {
-            lines.addStaticLine(Component.text("Lives", NamedTextColor.WHITE).decorate(TextDecoration.BOLD));
+            requiredLines += 2;
             for (Team team : MapController.getCurrentMap().teams) {
-                lines.addComponent(team);
+                if (team.getLives() >= 0) {
+                    requiredLines += 1;
+                }
             }
-            lines.addBlankLine();
         }
 
-        // Flip the order of flags, so they're in a more logical order
-        Flag[] reversedFlags = MapController.getCurrentMap().flags.clone();
-        ArrayUtils.reverse(reversedFlags);
-        // Display the flag scoreboard
-        for (Flag flag : reversedFlags) {
+        for (Flag flag : MapController.getCurrentMap().flags) {
             if (flag.isActive()) {
-                lines.addComponent(flag);
+                requiredLines++;
             }
         }
-
-
-        this.componentSidebar = new ComponentSidebarLayout(title, lines.build());
+        return requiredLines;
     }
 
     /**
@@ -88,6 +164,11 @@ public class FlagSidebar {
     public void tick() {
         // Advance title animation to the next frame
         titleAnimation.nextFrame();
+        pagesTicks++;
+        if (pagesTicks >= 12) {
+            pagesAnimation.nextFrame();
+            pagesTicks = 0;
+        }
 
         // Update sidebar title & lines
         componentSidebar.apply(sidebar);
