5a0927c3437001048a5643cbd67b64b5f1c252d0
Greenfoot5 <greenfoot89@gmail.com>
2025-01-20 14:22:32 +0000

    Added javadocs to TeamController

--- src/main/java/me/greenfoot5/castlesiege/maps/TeamController.java
+++ src/main/java/me/greenfoot5/castlesiege/maps/TeamController.java
@@ -9,29 +9,61 @@ import java.util.Optional;
 import java.util.UUID;
 
 /**
- * Checks if players are on the same team
+ * Manages the players on Castle Siege
  */
 public class TeamController implements FactionProvider {
 
     private static final HashMap<UUID, Team> uuidToTeam = new HashMap<>();
 
+    /**
+     * Adds a player to a team. If they are already on a team, removes the player from their team.
+     * @param uuid The UUID of the player to add to a team
+     * @param team The team to add the player to
+     */
     public static void joinTeam(UUID uuid, Team team) {
         uuidToTeam.put(uuid, team);
     }
 
+    /**
+     * Removes a player from their team
+     * @param uuid The UUID of the player to remove
+     */
     public static void leaveTeam(UUID uuid) {
         uuidToTeam.remove(uuid);
     }
 
+    /**
+     * Gets the team of a player
+     * @param uuid The UUID of the player
+     * @return The team if the player is on one. Returns null otherwise.
+     */
     public static Team getTeam(UUID uuid) {
         return uuidToTeam.get(uuid);
     }
 
+    public static void loadTeams() {
+        // We can and want to keep the teams the same
+        if (keepTeams && MapController.getCurrentMap().teams.length >= kept_teams.size()) {
+
+        }
+    }
+
+    /**
+     * Checks if a player is in a specific faction
+     * @param abstractPlayer The player to check for
+     * @param factionName The name of the faction (team)
+     * @return true if the player is in that faction
+     */
     @Override
     public boolean isInFaction(AbstractPlayer abstractPlayer, String s) {
         return Objects.equals(getTeam(abstractPlayer.getUniqueId()).getName(), s);
     }
 
+    /**
+     * Gets the team name of a player
+     * @param player The player to check for
+     * @return An Optional of the team's name
+     */
     @Override
     public Optional<String> getFaction(AbstractPlayer player) {
         String team = getTeam(player.getUniqueId()).getName();
@@ -39,6 +71,11 @@ public class TeamController implements FactionProvider {
         return Optional.empty();
     }
 
+    /**
+     * Gets the team name of a player
+     * @param uuid The player's UUID to check for
+     * @return An Optional of the team's name
+     */
     @Override
     public Optional<String> getFaction(UUID uuid) {
         String team = getTeam(uuid).getName();
