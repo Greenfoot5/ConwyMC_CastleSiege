2ae791ed179bcc47d8af595e0d4403f7091608fc
Greenfoot5 <greenfoot89@gmail.com>
2025-01-21 15:06:57 +0000

    Improved indenting in WoolMapBlock

--- src/main/java/me/greenfoot5/castlesiege/maps/WoolMapBlock.java
+++ src/main/java/me/greenfoot5/castlesiege/maps/WoolMapBlock.java
@@ -37,49 +37,19 @@ public class WoolMapBlock {
         Bukkit.getScheduler().runTaskAsynchronously(Main.plugin, () -> {
             Team team = TeamController.getTeam(uuid);
             Kit kit = Kit.equippedKits.get(uuid);
-                if (MapController.getCurrentMap() instanceof CoreMap coreMap) {
-                    Core core = coreMap.getCore(coreName);
-                    if (team != null && team.hasPlayer(uuid)) {
-                        if (!Objects.equals(core.getOwners(), team.getName())) {
-                            Messenger.sendActionError("Your team does not own this core.", player);
-                            return;
-                        } else if (kit == null) {
-                            Messenger.sendError("You can't join the battlefield without a kit/class!", player);
-                            Messenger.sendError("Choose a kit/class with the command <red>/kit</red>!", player);
-                            return;
-                        } else {
-                            Bukkit.getScheduler().runTask(Main.plugin, () -> {
-                                // Remove mount
-                                if (player.isInsideVehicle())
-                                    Objects.requireNonNull(player.getVehicle()).remove();
-                                // Set kit items
-                                Kit.equippedKits.get(uuid).setItems(uuid, true);
-                                // Spawn player
-                                player.teleport(core.getSpawnPoint());
-                                // Remove mount
-                                if (player.isInsideVehicle()) {
-                                    Objects.requireNonNull(player.getVehicle()).remove();
-                                }
-                                InCombat.playerSpawned(uuid);
-                            });
-                            Messenger.sendActionSpawn(core.name, team.primaryChatColor, team.secondaryChatColor, player);
-                        }
-                    }
-                }
 
-                if (MapController.getCurrentMap().getFlag(flagName) == null) {
-                    return;
-                }
-
-                Flag flag = MapController.getCurrentMap().getFlag(flagName);
+            if (MapController.getCurrentMap() instanceof CoreMap coreMap) {
+                Core core = coreMap.getCore(coreName);
                 if (team != null && team.hasPlayer(uuid)) {
-                    if (!Objects.equals(flag.getCurrentOwners(), team.getName())) {
-                        Messenger.sendActionError("Your team does not own this flag at the moment.", player);
-                    } else if (flag.underAttack()) {
-                        Messenger.sendActionError("You can't spawn here. This flag is under attack!", player);
+                    if (!Objects.equals(core.getOwners(), team.getName())) {
+                        Messenger.sendActionError("Your team does not own this core.", player);
+                        return;
+
                     } else if (kit == null) {
                         Messenger.sendError("You can't join the battlefield without a kit/class!", player);
                         Messenger.sendError("Choose a kit/class with the command <red>/kit</red>!", player);
+                        return;
+
                     } else {
                         Bukkit.getScheduler().runTask(Main.plugin, () -> {
                             // Remove mount
@@ -88,17 +58,54 @@ public class WoolMapBlock {
                             // Set kit items
                             Kit.equippedKits.get(uuid).setItems(uuid, true);
                             // Spawn player
-                            player.teleport(flag.getSpawnPoint(team.getName()));
+                            player.teleport(core.getSpawnPoint());
                             // Remove mount
                             if (player.isInsideVehicle()) {
                                 Objects.requireNonNull(player.getVehicle()).remove();
                             }
                             InCombat.playerSpawned(uuid);
-                            player.getWorld().playSound(flag.getSpawnPoint(team.getName()), Sound.BLOCK_ENCHANTMENT_TABLE_USE , 1, 1.1f);
                         });
-                        Messenger.sendActionSpawn(flagName, team.primaryChatColor, team.secondaryChatColor, player);
+                        Messenger.sendActionSpawn(core.name, team.primaryChatColor, team.secondaryChatColor, player);
                     }
                 }
+            }
+
+            Flag flag = MapController.getCurrentMap().getFlag(flagName);
+
+            if (flag == null) {
+                return;
+            }
+
+            if (team != null && team.hasPlayer(uuid)) {
+                if (!Objects.equals(flag.getCurrentOwners(), team.getName())) {
+                    Messenger.sendActionError("Your team does not own this flag at the moment.", player);
+
+                } else if (flag.underAttack()) {
+                    Messenger.sendActionError("You can't spawn here. This flag is under attack!", player);
+
+                } else if (kit == null) {
+                    Messenger.sendError("You can't join the battlefield without a kit/class!", player);
+                    Messenger.sendError("Choose a kit/class with the command <red>/kit</red>!", player);
+
+                } else {
+                    Bukkit.getScheduler().runTask(Main.plugin, () -> {
+                        // Remove mount
+                        if (player.isInsideVehicle())
+                            Objects.requireNonNull(player.getVehicle()).remove();
+                        // Set kit items
+                        Kit.equippedKits.get(uuid).setItems(uuid, true);
+                        // Spawn player
+                        player.teleport(flag.getSpawnPoint(team.getName()));
+                        // Remove mount
+                        if (player.isInsideVehicle()) {
+                            Objects.requireNonNull(player.getVehicle()).remove();
+                        }
+                        InCombat.playerSpawned(uuid);
+                        player.getWorld().playSound(flag.getSpawnPoint(team.getName()), Sound.BLOCK_ENCHANTMENT_TABLE_USE , 1, 1.1f);
+                    });
+                    Messenger.sendActionSpawn(flagName, team.primaryChatColor, team.secondaryChatColor, player);
+                }
+            }
         });
     }
 }
