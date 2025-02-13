a658b03d76a2fcc9650772fba72155d1da643fa8
Greenfoot5 <greenfoot89@gmail.com>
2025-02-13 22:53:55 +0000

    Tweaked cake to work with new media

--- src/main/java/me/greenfoot5/castlesiege/events/combat/EatCake.java
+++ src/main/java/me/greenfoot5/castlesiege/events/combat/EatCake.java
@@ -6,6 +6,7 @@ import me.greenfoot5.castlesiege.kits.kits.Kit;
 import me.greenfoot5.castlesiege.kits.kits.coin_kits.Medic;
 import me.greenfoot5.castlesiege.maps.TeamController;
 import me.greenfoot5.castlesiege.misc.CSNameTag;
+import me.greenfoot5.conwymc.data_types.Tuple;
 import me.greenfoot5.conwymc.util.Messenger;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -20,7 +21,9 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.UUID;
 
 /**
  * Handles cake buffs
@@ -29,6 +32,8 @@ public class EatCake implements Listener {
 
     public static final int CAKE_DURATION = 160;
     public static final int REGEN_AMPLIFIER = 12;
+
+    public static HashSet<UUID> cooldowns;
 
     /**
      * Take a bite from a cake and gain a short period of regeneration.
@@ -53,14 +58,15 @@ public class EatCake implements Listener {
 
             // Get the player who placed the cake
             Block cake = event.getClickedBlock();
-            Player placer = Medic.getPlacer(cake);
 
-            if (canEatCake(eater, placer)) {
+            Tuple<Boolean, Player> canEat = canEatCake(eater);
+
+            if (canEat.getFirst()) {
                 // Send messages and award heal
-                if (placer != null && !Objects.equals(eater, placer)) {
-                    Messenger.sendActionInfo(CSNameTag.mmUsername(placer) + "'s cake is healing you!", eater);
-                    Messenger.sendActionSuccess("Your cake is healing " + CSNameTag.mmUsername(eater), placer);
-                    UpdateStats.addHeals(placer.getUniqueId(), 1);
+                if (canEat.getSecond() != null && !Objects.equals(eater, canEat.getSecond())) {
+                    Messenger.sendActionInfo(CSNameTag.mmUsername(canEat.getSecond()) + "'s cake is healing you!", eater);
+                    Messenger.sendActionSuccess("Your cake is healing " + CSNameTag.mmUsername(eater), canEat.getSecond());
+                    UpdateStats.addHeals(canEat.getSecond().getUniqueId(), 1);
 
                 } else {
                     Messenger.sendHealing("The cake is healing you!", eater);
@@ -73,7 +79,6 @@ public class EatCake implements Listener {
 
                     if (cakeData.getBites() >= cakeData.getMaximumBites()) {
                         cake.breakNaturally();
-                        Medic.cakes.remove(placer);
                     } else {
                         cakeData.setBites(cakeData.getBites() + 1);
                         cake.setBlockData(cakeData);
@@ -86,26 +91,33 @@ public class EatCake implements Listener {
     /**
      * Check if the player can eat from the cake.
      * @param eater The player who attempts to eat the cake
-     * @param placer The player who placed the cake
      * @return Whether the player can eat the cake
      */
-    private synchronized boolean canEatCake(Player eater, Player placer) {
+    private synchronized Tuple<Boolean, Player> canEatCake(Player eater) {
+        Player placer = null;
+        for (Kit kit : Kit.equippedKits.values()) {
+            if (kit instanceof Medic) {
+                if (kit.getEquippedPlayer() != null)
+                    placer = kit.getEquippedPlayer();
+            }
+        }
+
         // The cake was placed by an enemy
         if (placer != null && TeamController.getTeam(eater.getUniqueId())
                 != TeamController.getTeam(placer.getUniqueId())) {
             Messenger.sendActionError("This is an enemy cake, destroy it!", eater);
-            return false;
+            return new Tuple<>(false, placer);
         }
 
         // The eater has full health or is on cooldown
         if (eater.getHealth() == Kit.equippedKits.get(eater.getUniqueId()).baseHealth
-                || Medic.cooldown.contains(eater)) {
-            return false;
+                || cooldowns.contains(eater.getUniqueId())) {
+            return new Tuple<>(false, placer);
         }
 
         // Apply cooldown
-        Medic.cooldown.add(eater);
-        Bukkit.getScheduler().runTaskLaterAsynchronously(Main.plugin, () -> Medic.cooldown.remove(eater), CAKE_DURATION - 1);
-        return true;
+        cooldowns.add(eater.getUniqueId());
+        Bukkit.getScheduler().runTaskLaterAsynchronously(Main.plugin, () -> cooldowns.remove(eater.getUniqueId()), CAKE_DURATION - 1);
+        return new Tuple<>(true, placer);
     }
 }
