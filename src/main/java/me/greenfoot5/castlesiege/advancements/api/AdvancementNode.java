a67756438ccdde6a3b25fb125eed47ef8011b65f
Greenfoot5 <greenfoot89@gmail.com>
2025-03-08 18:44:50 +0000

    Added many javadocs for AdvancementNode

--- src/main/java/me/greenfoot5/castlesiege/advancements/api/AdvancementNode.java
+++ src/main/java/me/greenfoot5/castlesiege/advancements/api/AdvancementNode.java
@@ -13,6 +13,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * A node allowing automatic display of advancements
+ */
 @SuppressWarnings("deprecation")
 public class AdvancementNode {
     private final String key;
@@ -66,6 +69,11 @@ public class AdvancementNode {
         }
     }
 
+    /**
+     * Finds a node in the node or it's parents
+     * @param key The key to use when searching for the node
+     * @return The first node with the specified key
+     */
     // Gets a node (either itself or child) based on key
     public AdvancementNode find(String key) {
         key = NodeDisplay.cleanKey(key);
@@ -112,6 +120,12 @@ public class AdvancementNode {
         return new Tuple<>(displays, y_size);
     }
 
+    /**
+     * Creates advancements from the node
+     * @param tab The tab use create the advancements in
+     * @param background The background image to use
+     * @return The created advancements
+     */
     public Tuple<RootAdvancement, BaseAdvancement[]> asAdvancementList(AdvancementTab tab, String background) {
         HashMap<String, NodeDisplay> displays = getDisplays();
 
@@ -149,6 +163,9 @@ public class AdvancementNode {
         return new Tuple<>(root, advancements.values().toArray(new BaseAdvancement[0]));
     }
 
+    /**
+     * Builds new advancement nodes
+     */
     public static class AdvancementNodeBuilder {
         private final String key;
 
@@ -176,71 +193,138 @@ public class AdvancementNode {
         // Additionals
         private int maxProgress = 1;
 
+        /**
+         * Create a new advancement
+         * @param key The key of the new node
+         */
         public AdvancementNodeBuilder(String key) {
             this(key, null);
         }
 
+        /**
+         * Create a new advancement
+         * @param key The key of the new node
+         * @param parent The key of the parent node
+         */
         public AdvancementNodeBuilder(String key, String parent) {
             this.key = key;
-
             this.parent = parent;
         }
 
+        /**
+         * Sets the title text of the node
+         * @param title MM string of the title
+         * @return The node builder with the title set
+         */
         public AdvancementNodeBuilder setTitle(String title) {
             this.title = title;
             return this;
         }
 
+        /**
+         * Sets the description of the node
+         * @param description The MM description
+         * @return The node builder with the description set
+         */
         public AdvancementNodeBuilder setDescription(String description) {
             this.description = description;
             return this;
         }
 
+        /**
+         * Sets the requirements to allow auto-creation of description
+         * @param requirements The MM requirements
+         * @return The node builder with the requirements set
+         */
         public AdvancementNodeBuilder setRequirements(String[] requirements) {
             this.requirements = requirements;
             return this;
         }
 
+        /**
+         * Sets the reward to allow auto-creation of description
+         * @param reward  The MM reward text
+         * @return The node builder with the reward set
+         */
         public AdvancementNodeBuilder setReward(String reward) {
             this.reward = reward;
             return this;
         }
 
+        /**
+         * Sets the material for the advancement
+         * @param material The Material item to use as an icon
+         * @return The node builder with the material set
+         */
         public AdvancementNodeBuilder setMaterial(Material material) {
             this.material = material;
             return this;
         }
 
+        /**
+         * Sets the frame
+         * @param frameType The frame to use on the advancement
+         * @return The node builder with the frame type set
+         */
         public AdvancementNodeBuilder setFrameType(AdvancementFrameType frameType) {
             this.frameType = frameType;
             return this;
         }
 
+        /**
+         * Sets the maximum progress of the advancement
+         * @param maxProgress The maximum progress the advancement requires
+         * @return The node builder with the maximum progress set
+         */
         public AdvancementNodeBuilder setMaxProgress(int maxProgress) {
             this.maxProgress = maxProgress;
             return this;
         }
 
+        /**
+         * Sets the announcement message
+         * @param announceMessage Bungee BaseComponent message
+         * @return The node builder with the announcement message set
+         */
         public AdvancementNodeBuilder setAnnounceMessage(BaseComponent[] announceMessage) {
             this.announceMessage = announceMessage;
             return this;
         }
 
+        /**
+         * Sets if the advancement should announce it in chat
+         * @param announceInChat true if it should be announced
+         * @return The node builder with announce in chat set
+         */
         public AdvancementNodeBuilder setAnnounceInChat(boolean announceInChat) {
             this.announceInChat = announceInChat;
             return this;
         }
 
+        /**
+         * Sets the compact description of the advancement
+         * @param compactDescription The string compact description
+         * @return The node builder with the compact description set
+         */
         public AdvancementNodeBuilder setCompactDescription(String compactDescription) {
             this.compactDescription = compactDescription;
             return this;
         }
 
+        /**
+         * If upon getting the advancement it should show the toast
+         * @param showToast If the toast should be shown
+         * @return The node builder with show toast set
+         */
         public AdvancementNodeBuilder setShowToast(boolean showToast) {
             this.showToast = showToast;
             return this;
         }
 
+        /**
+         * Builds the advancement node
+         * @return The built advancement node
+         */
         public AdvancementNode build()
         {
             if (requirements != null) {
